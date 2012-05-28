5c221e594d8d259541f4e1cb69554a74d76b6af0
garfieldchen <ericchn007@gmail.com>
2012-05-28 08:37:13 +0800

    init &, could eccode 8 bit len, decode is not ready

---
+++ HuffmanC/FileBuffer.cpp
@@ -0,0 +1,139 @@
+#include "FileBuffer.h"
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <memory.h>
+#include <algorithm>
+
+FileReadBuffer::FileReadBuffer(const char* fn, size_t bufferSize) 
+	:capacity(bufferSize)
+{
+	buffer = (byte*)malloc(bufferSize);
+	ptr = buffer;
+	realSize = 0;
+	bitOffset = 0;
+
+	file = fopen(fn, "rb");
+}
+
+FileReadBuffer::~FileReadBuffer() {
+	if (file)
+		fclose(file);
+	free(buffer);
+}
+
+BufferData FileReadBuffer::read(size_t size) {
+	if (!file)
+		return BufferData();
+
+	const size_t availableSize = (size_t)(realSize - (size_t)(ptr - buffer));
+	if (availableSize < size ) {
+		if (availableSize > 0 )
+			memcpy(buffer, ptr, availableSize);
+		size_t readed = fread(buffer + availableSize, 1, capacity - availableSize, file);
+		ptr = buffer;
+		realSize = readed + availableSize;
+
+		if (realSize < size)
+			return BufferData();
+	}
+
+	ptr += size;
+	return BufferData(ptr, size);
+}
+
+Bit FileReadBuffer::readBit(size_t size)
+{
+	Bit retBit;
+
+	if (bitOffset>0) {
+		const size_t availableCnt = 8 - bitOffset;
+		const size_t usedCnt = std::min(size, availableCnt);
+		
+		retBit.add((long)(*ptr) >> bitOffset, usedCnt);
+		bitOffset = (bitOffset + usedCnt) % 8;
+		size -= bitOffset;
+
+		if (size <= availableCnt)
+			return retBit;	
+	}
+
+	const size_t byteCnt = size / 8 + (size % 8 ? 1 : 0); 
+	BufferData data = read(byteCnt);
+	if (data.size < byteCnt)
+		return Bit();
+	bitOffset = size % 8;
+	
+	retBit.add(*((long*)data.buffer), byteCnt); // TODO there will be a bug, read invalid address
+	return retBit;
+}
+
+size_t FileReadBuffer::write(byte* data, size_t size)
+{
+	assert(0);
+	return 0;
+}
+
+size_t FileReadBuffer::writeBit(const Bit& bit) {
+	assert(0);
+	return 0;
+}
+
+void FileReadBuffer::rewind() {
+	fseek(file, 0, SEEK_SET);
+	ptr = buffer;
+	realSize = 0;
+	bitOffset = 0;
+}
+///////////////////////
+FileWriteBuffer::FileWriteBuffer(const char* fn, size_t bufferSize)
+	:capacity(bufferSize)
+{
+	buffer = (byte*)malloc(capacity);
+	file = fopen(fn, "wb+");
+	ptr = buffer;
+	byteOffset = 0;
+}
+
+FileWriteBuffer::~FileWriteBuffer() {
+	if (ptr != buffer)
+		fwrite(buffer, ptr - buffer, 1, file);
+
+	free(buffer);
+	fclose(file);
+}
+
+BufferData FileWriteBuffer::read(size_t size) {
+	assert(0);
+	return BufferData();
+}
+
+size_t FileWriteBuffer::write(byte* data, size_t size) {
+	assert(size <= capacity);
+	size_t copySize = std::min(size, capacity - (ptr - buffer));
+	
+	memcpy(ptr, data, copySize);
+	if (ptr == (buffer + capacity)) {
+		fwrite(buffer, capacity, 1, file);
+		ptr = buffer;
+	}
+
+	const size_t restSize = size - copySize;
+	if (restSize > 0) {
+		memcpy(buffer, data + copySize, restSize);
+		ptr = buffer + restSize;
+	}
+	return size;
+}
+
+size_t FileWriteBuffer::writeBit(const Bit& bit)
+{
+	long bitBuffer = bit.bits;
+
+	return 0;
+}
+
+Bit FileWriteBuffer::readBit(size_t size) {
+	assert(0);
+	return Bit();
+}
